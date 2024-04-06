7980c50c0f0080d1a4939f0d5317a5ddc7292466
install-java <91494580+install-java@users.noreply.github.com>
2024-04-06 16:04:33 +0200

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -5,6 +5,58 @@ def on_button_pressed_a():
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 
+
+class Gamestate(Enum):
+    PLAYER_SEL = 1
+    CARD_SEL = 2
+
+class Spiel:
+    mode = 0
+    modeIndex = 0
+    numberOfCards = 0
+    gamestate = Gamestate.PLAYER_SEL
+    cards = []
+    drawnCards = []
+
+    def __init__(self):
+        pass
+
+    def selectMode():
+        pass
+
+    def incrementMode():
+        Spiel.modeIndex += 1
+
+    def decrementMode():
+        Spiel.modeIndex -= 1
+
+    def confirmMode():
+        pass
+
+    def selectNumberOfCards():
+        Spiel.numberOfCards = Spiel.modeIndex
+
+    def incrementNumberOfCards():
+        Spiel.modeIndex += 1
+
+    def decrementNumberOfCards():
+        Spiel.modeIndex -= 1
+
+    def celebration():
+        pass
+
+    def userInducedExit():
+        pass
+
+    def drawCard():
+        pass
+
+    def outputCard():
+        pass
+
+    def exitGame():
+        pass
+
 class Newtest:
     def run_func(self):
         return "string"
--- main.ts
+++ main.ts
@@ -3,6 +3,170 @@ input.onButtonPressed(Button.A, function on_button_pressed_a() {
     test.x = test.run_func()
     console.log(test.x)
 })
+class Gamestate {
+    static PLAYER_SEL: number
+    private ___PLAYER_SEL_is_set: boolean
+    private ___PLAYER_SEL: number
+    get PLAYER_SEL(): number {
+        return this.___PLAYER_SEL_is_set ? this.___PLAYER_SEL : Gamestate.PLAYER_SEL
+    }
+    set PLAYER_SEL(value: number) {
+        this.___PLAYER_SEL_is_set = true
+        this.___PLAYER_SEL = value
+    }
+    
+    static CARD_SEL: number
+    private ___CARD_SEL_is_set: boolean
+    private ___CARD_SEL: number
+    get CARD_SEL(): number {
+        return this.___CARD_SEL_is_set ? this.___CARD_SEL : Gamestate.CARD_SEL
+    }
+    set CARD_SEL(value: number) {
+        this.___CARD_SEL_is_set = true
+        this.___CARD_SEL = value
+    }
+    
+    public static __initGamestate() {
+        Gamestate.PLAYER_SEL = 1
+        Gamestate.CARD_SEL = 2
+    }
+    
+}
+
+Gamestate.__initGamestate()
+
+class Spiel {
+    static modeIndex: number
+    private ___modeIndex_is_set: boolean
+    private ___modeIndex: number
+    get modeIndex(): number {
+        return this.___modeIndex_is_set ? this.___modeIndex : Spiel.modeIndex
+    }
+    set modeIndex(value: number) {
+        this.___modeIndex_is_set = true
+        this.___modeIndex = value
+    }
+    
+    static numberOfCards: number
+    private ___numberOfCards_is_set: boolean
+    private ___numberOfCards: number
+    get numberOfCards(): number {
+        return this.___numberOfCards_is_set ? this.___numberOfCards : Spiel.numberOfCards
+    }
+    set numberOfCards(value: number) {
+        this.___numberOfCards_is_set = true
+        this.___numberOfCards = value
+    }
+    
+    static mode: number
+    private ___mode_is_set: boolean
+    private ___mode: number
+    get mode(): number {
+        return this.___mode_is_set ? this.___mode : Spiel.mode
+    }
+    set mode(value: number) {
+        this.___mode_is_set = true
+        this.___mode = value
+    }
+    
+    static gamestate: number
+    private ___gamestate_is_set: boolean
+    private ___gamestate: number
+    get gamestate(): number {
+        return this.___gamestate_is_set ? this.___gamestate : Spiel.gamestate
+    }
+    set gamestate(value: number) {
+        this.___gamestate_is_set = true
+        this.___gamestate = value
+    }
+    
+    static cards: any[]
+    private ___cards_is_set: boolean
+    private ___cards: any[]
+    get cards(): any[] {
+        return this.___cards_is_set ? this.___cards : Spiel.cards
+    }
+    set cards(value: any[]) {
+        this.___cards_is_set = true
+        this.___cards = value
+    }
+    
+    static drawnCards: any[]
+    private ___drawnCards_is_set: boolean
+    private ___drawnCards: any[]
+    get drawnCards(): any[] {
+        return this.___drawnCards_is_set ? this.___drawnCards : Spiel.drawnCards
+    }
+    set drawnCards(value: any[]) {
+        this.___drawnCards_is_set = true
+        this.___drawnCards = value
+    }
+    
+    public static __initSpiel() {
+        Spiel.mode = 0
+        Spiel.modeIndex = 0
+        Spiel.numberOfCards = 0
+        Spiel.gamestate = Gamestate.PLAYER_SEL
+        Spiel.cards = []
+        Spiel.drawnCards = []
+    }
+    
+    constructor() {
+        
+    }
+    
+    public static selectMode() {
+        
+    }
+    
+    public static incrementMode() {
+        Spiel.modeIndex += 1
+    }
+    
+    public static decrementMode() {
+        Spiel.modeIndex -= 1
+    }
+    
+    public static confirmMode() {
+        
+    }
+    
+    public static selectNumberOfCards() {
+        Spiel.numberOfCards = Spiel.modeIndex
+    }
+    
+    public static incrementNumberOfCards() {
+        Spiel.modeIndex += 1
+    }
+    
+    public static decrementNumberOfCards() {
+        Spiel.modeIndex -= 1
+    }
+    
+    public static celebration() {
+        
+    }
+    
+    public static userInducedExit() {
+        
+    }
+    
+    public static drawCard() {
+        
+    }
+    
+    public static outputCard() {
+        
+    }
+    
+    public static exitGame() {
+        
+    }
+    
+}
+
+Spiel.__initSpiel()
+
 class Newtest {
     static x: string
     private ___x_is_set: boolean
